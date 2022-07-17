f17b4329dae63dfc18b05814a5a26f9c58e13f32
lior alafi <lalafi@armosec.io>
2022-07-17 09:39:32 +0300

    switched to assert

--- registries/quay/quayio_test.go
+++ registries/quay/quayio_test.go
@@ -9,52 +9,38 @@ import (
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/stretchr/testify/assert"
 )
 
 //those are integration tests
-// TODO: replace with asserts
 func TestSimpleNoAuth(t *testing.T) {
 	registry, err := name.NewRegistry("quay.io")
-	if err != nil {
-		t.Errorf("err1: %v", err.Error())
-	}
+	assert.Nil(t, err, "failed to create registry")
+
 	quayio, err := NewQuayIORegistry(nil, &registry, &common.RegistryOptions{})
 	ctx := context.Background()
 	repos, _, err := quayio.Catalog(ctx, common.NoPaginationOption(), common.CatalogOption{IsPublic: true, Namespaces: "quay"}, nil)
-	if err != nil {
-		t.Errorf("%v", err.Error())
-	}
+	assert.Nil(t, err, "failed to catalog")
+	assert.NotEmpty(t, repos, "expected some returned images")
 
-	if len(repos) == 0 {
-		t.Error("expected more errors")
-	}
-	for _, repo := range repos {
-		fullRepoName := quayio.GetRegistry().Name() + "/" + repo
-		repo_data, err := name.NewRepository(fullRepoName)
-		if err != nil {
-			t.Error("basa")
-		}
-		imagestags, err := remote.List(repo_data, remote.WithAuth(authn.Anonymous))
-		if err != nil {
-			t.Error("basa2")
-		}
-		fmt.Printf("%s/%v\n", fullRepoName, imagestags)
-	}
+	repo := repos[0]
+	fullRepoName := quayio.GetRegistry().Name() + "/" + repo
+	repo_data, err := name.NewRepository(fullRepoName)
+	assert.Nil(t, err, "failed to create repo data")
+
+	imagestags, err := remote.List(repo_data, remote.WithAuth(authn.Anonymous))
+	assert.Nil(t, err, "failed to list image tags")
+	assert.NotEmpty(t, imagestags, fmt.Errorf("expected tags for %s", fullRepoName))
 }
 
-func TestSimpleAuth(t *testing.T) {
-	registry, err := name.NewRegistry("quay.io")
-	if err != nil {
-		t.Errorf("err1: %v", err.Error())
-	}
-	quayio, err := NewQuayIORegistry(&authn.AuthConfig{Username: "armosec+cajenkins", Password: "D4TEPRNY57BH8RA6KMCNRJ4V462PA1EUXJ8H2SM4OYJW12FMOJIEBNUH8MY65B6S"}, &registry, common.MakeRegistryOptions(false, false, false, "quay.io", "", "", common.Quay))
-	ctx := context.Background()
-	data, _, err := quayio.Catalog(ctx, common.NoPaginationOption(), common.CatalogOption{Namespaces: "armosec"}, nil)
-	if err != nil {
-		t.Errorf("%v", err)
-		return
-	}
+// func TestSimpleAuth(t *testing.T) {
+// 	registry, err := name.NewRegistry("quay.io")
+// 	assert.Nil(t, err, err.Error())
 
-	t.Errorf("%v", data)
+// 	quayio, err := NewQuayIORegistry(&authn.AuthConfig{Username: "", Password: ""}, &registry, common.MakeRegistryOptions(false, false, false, "quay.io", "", "", common.Quay))
+// 	ctx := context.Background()
+// 	data, _, err := quayio.Catalog(ctx, common.NoPaginationOption(), common.CatalogOption{Namespaces: "armosec"}, nil)
+// 	assert.Nil(t, err, err.Error())
+// 	assert.NotEqual(t, len(data), 0, "received an empty results")
 
-}
+// }
